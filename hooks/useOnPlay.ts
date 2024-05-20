b8a9474ce594ef352636275a129d442a02dfc859
rkeerthick <rkeerthick1@gmail.com>
2024-05-20 23:50:48 +0530

    unwanted subscription deleted

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -17,9 +17,9 @@ const useOnPlay = (songs: Song[]) => {
             return authModal.onOpen();
         }
 
-        if(!subscription) {
-            return subscribeModal.onOpen();
-        }
+        // if(!subscription) {
+        //     return subscribeModal.onOpen();
+        // }
 
         player.setId(id);
         player.setIds(songs.map((song) => song.id))
